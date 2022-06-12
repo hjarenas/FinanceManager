0cb3f2578d3cff25dde39b954f951bfd95c80ad0
Hector <hector.it.dev@gmail.com>
2022-06-12 12:40:05 +0200

    apply migrations

---
+++ src/FinanceManager.Infrastructure/Persistence/Migrations/20220612103221_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceManager.Infrastructure.Persistence.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Expenses",
+                columns: table => new
+                {
+                    ExpenseId = table.Column<Guid>(type: "uuid", nullable: false),
+                    BookingDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Recipient = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    IntendedUse = table.Column<string>(type: "text", nullable: false),
+                    Direction = table.Column<int>(type: "integer", nullable: false),
+                    AmountInEur = table.Column<decimal>(type: "numeric", nullable: false),
+                    Category_Name = table.Column<string>(type: "text", nullable: false),
+                    BankAccount_BankName = table.Column<string>(type: "text", nullable: false),
+                    BankAccount_Isin = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expenses", x => x.ExpenseId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Expenses");
+        }
+    }
+}
--- src/FinanceManager.Infrastructure/README.md
+++ src/FinanceManager.Infrastructure/README.md
@@ -1,13 +1,23 @@
-### Database Migrations
+# Database Migrations
 
+## Generating the migrations
 To use `dotnet-ef` for your migrations first ensure that "UseInMemoryDatabase" is disabled, as described within previous section.
 Then, add the following flags to your command (values assume you are executing from repository root)
 
 * `--project src/FinanceManager.Infrastructure` (optional if in this folder)
 * `--startup-project src/FinanceManager.WebAPI`
-* `--output-dir src/FinanceManager.Infrastructure/Persistence/Migrations`
+* `--output-dir Persistence/Migrations`
 
 The full command would look like this:
 ``` bash
-dotnet ef migrations add InitialCreate --project src/FinanceManager.Infrastructure --startup-project src/FinanceManager.WebAPI --output-dir src/FinanceManager.Infrastructure/Persistence/Migrations
+dotnet ef migrations add InitialCreate --project src/FinanceManager.Infrastructure --startup-project src/FinanceManager.WebAPI --output-dir Persistence/Migrations
+```
+## Applying the migrations
+According to [Microsoft](https://docs.microsoft.com/en-us/ef/core/managing-schemas/migrations/applying) the recommended way
+is to generate the SQL scripts with the cli tool.
+
+It is possible to apply the migrations using the command line tool though. In order to apply the migrations,
+we only need to run the following command:
+``` bash
+dotnet ef database update --project src/FinanceManager.WebAPI/
 ```
