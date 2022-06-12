a571bce10a33367817f49667118aab840c265717
Hector <hector.it.dev@gmail.com>
2022-06-12 13:36:22 +0200

    Treat enums as strings

--- src/FinanceManager.Infrastructure/Persistence/Configuration/ExpensesConfiguration.cs
+++ src/FinanceManager.Infrastructure/Persistence/Configuration/ExpensesConfiguration.cs
@@ -1,12 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using FinanceManager.Domain.ExpensesAggregate;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FinanceManager.Infrstructure.Persistence.Configuration;
 public class ExpensesConfiguration : IEntityTypeConfiguration<Expense>
 {
     public void Configure(EntityTypeBuilder<Expense> builder)
     {
+        var converter = new EnumToStringConverter<Direction>();
+        builder.Property(e => e.Direction)
+            .HasConversion(converter);
         builder.OwnsOne(e => e.BankAccount);
         builder.OwnsOne(e => e.Category);
     }
---
+++ src/FinanceManager.Infrastructure/Persistence/Migrations/20220612112103_TreatEnumsAsStrings.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceManager.Infrastructure.Persistence.Migrations
+{
+    public partial class TreatEnumsAsStrings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Direction",
+                table: "Expenses",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Direction",
+                table: "Expenses",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
